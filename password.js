23ea70812a18956d8414b0a668e1dfb7e08fffe3
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-17 22:26:39 -0500

    Add files via upload

--- password.js
+++ password.js
@@ -1,26 +1,14 @@
-console.log('Welcome to password validator tool.')
+console.log('Welcome to password validator tool.');
 
-var myInput = document.getElementById('password');
-var number = document.getElementById("number");
-var length = document.getElementById("length");
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 
 
-var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) { 
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-
-  }
-
-  if(myInput.value.length >= 8) {
-        length.classList.remove("invalid");
-        length.classList.add("valid");
-      } else {
-        length.classList.remove("valid");
-        length.classList.add("invalid");
-      }
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
     
